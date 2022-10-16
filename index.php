4a60b4fa02a9cec6343f0cef838f453a086c05d2
Rob Hamilton <robertkhamilton@users.noreply.github.com>
2022-10-16 15:33:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if (($handle = fopen("main.csv", "r")) !== FALSE) {
         $num = count($data);
         $row++;
         for ($c=0; $c < $num; $c++) {
-            echo "<div>$data[$c] . </div>\n";
+            echo "<div class='arts_class'>$data[$c] . </div>\n";
             
         }
     }
