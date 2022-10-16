294fd7d465a29e158a8402df062d8641c0458735
Rob Hamilton <robertkhamilton@users.noreply.github.com>
2022-10-16 15:41:59 -0400

    Update index.php

--- index.php
+++ index.php
@@ -11,10 +11,24 @@
 
 <BODY>
     
-  
-<?php Header (“Content-type: text/css; charset=utf-8”);?>
 
+<?php 
 
+$row = 1;
+if (($handle = fopen("main.csv", "r")) !== FALSE) {
+    
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo "<div>$data[$c] . </div>\n";
+            
+        }
+    }
+    fclose($handle);
+
+}
+?>   
 
 
 
