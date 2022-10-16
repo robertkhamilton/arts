ded774d541c59c83e30e296e9baf81fb091ae756
Rob Hamilton <robertkhamilton@users.noreply.github.com>
2022-10-16 15:09:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php include_once("index.html"); ?>
 
 <?php include_once("main.csv"); ?>
+
+$row = 1;
+if (($handle = fopen("main.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
